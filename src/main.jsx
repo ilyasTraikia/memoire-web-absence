3327be5eaffa481cb45d10835a9873691f09cdad
Amine Ayachi <32771441+AmineAyachi@users.noreply.github.com>
2023-04-15 22:12:37 +0100

    added sidebar and header

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
 import App from './main/App'
 import './style/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App />,
+  },
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
